1943bc30be056b218a049cda653b12a9b56198bb
Brenton <brenton@math.umd.edu>
2016-05-27 11:18:31 +0200

    hokey scripts for running with a bunch of params

---
+++ scripts/run-sq-wN-jobs.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+export W=5
+
+export S=1.0
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+
+export S=0.9
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+
+export S=0.8
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+
+export S=0.7
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+export S=0.6
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+export S=0.5
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+export S=0.4
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+export S=0.3
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+export S=0.2
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
+export S=0.1
+export A=0.1
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.2
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.3
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.4
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.5
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.6
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.7
+java -Xmx5g -cp bin forkulator.FJSimulator s $W $W $A $S 1000000000 1000 data/"sq_w"$W"_t2_a"$a"_s"$S"_n1000000000" >> data/"sq_job_means_w"$W"_s"$S".dat"
+export A=0.8
+
