c8594c1bc3ec58674a935c39aa0218ba04b04277
Brenton Walker <brenton>
2016-06-17 14:54:02 +0200

    refactored the service process code throughout.  Now any IntertimeProcess can be passed in.

--- src/forkulator/FJJob.java
+++ src/forkulator/FJJob.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class FJJob {
 
-	public static final boolean ERLANG = false;
 	private static long ID_counter = 0;
 	public long ID = ID_counter++;
 	
@@ -15,15 +14,33 @@ public class FJJob {
 	private int task_index = 0;
 	public boolean complete = false;
 	
-	public FJJob(int num_tasks, double service_rate) {
-		if (ERLANG) {
-			this.num_tasks = 1;
-		} else {
-			this.num_tasks = num_tasks;
+	public static ArrayList<IntertimeProcess> service_processes = null;
+	
+	public FJJob(int num_tasks, IntertimeProcess service_process, double arrival_time) {
+		this.num_tasks = num_tasks;
+		
+		// in the first call set up an independent service_process for each
+		// task "channel".  That is, task 1 will always come from the same
+		// service process, all task 2's will etc...
+		// If the number of service processes allocated before is not enough,
+		// add more.
+		// I am not really happy with this implicit initialization.
+		if (service_processes == null) {
+			service_processes = new ArrayList<IntertimeProcess>(num_tasks);
+			System.err.println("service_processes.size="+service_processes.size());
+			for (int i=0; i<num_tasks; i++) {
+				service_processes.add(i, service_process.clone());
+			}
+		} else if (service_processes.size() < num_tasks) {
+			service_processes.ensureCapacity(num_tasks);
+			for (int i=service_processes.size(); i<num_tasks; i++) {
+				service_processes.add(i, service_process.clone());
+			}
 		}
+		
 		tasks = new FJTask[this.num_tasks];
 		for (int i=0; i<this.num_tasks; i++) {
-			tasks[i] = new FJTask(num_tasks, service_rate);
+			tasks[i] = new FJTask(service_processes.get(i));
 		}
 	}
 	
--- src/forkulator/FJTask.java
+++ src/forkulator/FJTask.java
@@ -1,10 +1,6 @@
 package forkulator;
 
-import java.util.Random;
-
 public class FJTask {
-
-	private static Random rand = new Random();
 
 	private static long ID_counter = 0;
 	public long ID = ID_counter++;
@@ -16,18 +12,8 @@ public class FJTask {
 	public boolean processing = false;
 	public boolean completed = false;
 	
-	public FJTask(int num_tasks, double service_rate) {
-		if (FJJob.ERLANG) {
-			// this version gives Erlang service times
-			double p = 1.0;
-			for (int i=0; i<num_tasks; i++) {
-				p *= rand.nextDouble();
-			}
-			this.service_time = -Math.log(p)/service_rate;
-		} else {
-			// exponential service times
-			this.service_time = -Math.log(rand.nextDouble())/service_rate;
-		}
+	public FJTask(IntertimeProcess service_process) {
+		this.service_time = service_process.nextInterval();
 	}
 	
 	
