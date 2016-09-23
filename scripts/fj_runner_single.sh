17a95ca1355855dc870b32b52610683558dfab26
ikt@sparkle4 <ikt@sparkle4>
2016-09-23 11:06:09 +0200

    new single-job runer script

---
+++ scripts/fj_runner_single.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -Xmx5g -cp "bin:lib/commons-math3-3.6.1.jar:lib/commons-cli-1.3.1.jar" forkulator.FJSimulator -q $1 -A x $2 -S x 1.0 -w $4 -t $4 -n $5 -i 100 -o new-fjpaper-data/fj_$1q_l$3_mu10_w$4t$4 >> new-fjpaper-data/fj_$1q_l$3_means.dat
