178af306c78bbbed4d8960c4bea342135ea5766e
Brenton Walker <brenton.d.walker@gmail.com>
2016-09-30 10:41:32 +0200

    make the service times in stages of multi-stage server optionally independent from one stage to another.  Before they were identical at each stage, which was not the intention.

--- src/forkulator/FJTask.java
+++ src/forkulator/FJTask.java
@@ -12,13 +12,40 @@ public class FJTask {
 	public boolean processing = false;
 	public boolean completed = false;
 	public FJJob job = null;
+	public IntertimeProcess service_process = null;
 	
+	/**
+	 * Constuctor
+	 * 
+	 * @param service_process
+	 * @param arrival_time
+	 * @param job
+	 */
 	public FJTask(IntertimeProcess service_process, double arrival_time, FJJob job) {
+		this.service_process = service_process;
 		this.service_time = service_process.nextInterval(arrival_time);
 		this.job = job;
 	}
 	
 	
+	/**
+	 * Generate a new service time from the service time process.
+	 * This is useful for multi-stage systems with independent stages.
+	 */
+	public void resampleServiceTime() {
+		service_time = service_process.nextInterval();
+	}
 	
 	
+	/**
+	 * Generate a new service time from the service time process.
+	 * This is useful for multi-stage systems with independent stages.
+	 * 
+	 * This version passes the arrival time to the service time process,
+	 * in case you are using leaky buckets or something.
+	 */
+	public void resampleServiceTime(double arrival_time) {
+		service_time = service_process.nextInterval(arrival_time);
+	}
+
 }
