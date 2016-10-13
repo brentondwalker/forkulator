b45ec6ce4fc136636663c782815373e3541839b2
Brenton Walker <brenton.d.walker@gmail.com>
2016-10-13 13:34:29 +0200

    working on spark, but only with single-core workers.  With multi-core workers the static data in the classes causes conflicts

--- src/main/java/FJSparkSimulator.java
+++ src/main/java/FJSparkSimulator.java
@@ -1,9 +1,16 @@
 package forkulator;
 
 import org.apache.spark.api.java.JavaSparkContext;
+//import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 import org.apache.spark.api.java.JavaRDD;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.zip.GZIPOutputStream;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -14,17 +21,47 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.apache.spark.SparkConf;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.util.zip.GZIPOutputStream;
 
+
+/**
+ * Simulate a Fork-Join system on a spark cluster!
+ * 
+ * ./bin/spark-submit --master spark://172.23.27.20:7077  --conf spark.cores.max=2 --class forkulator.FJSparkSimulator /home/brenton/properbounds/forkulator-sbt/target/scala-2.10/forkulator-assembly-1.0.jar -q w -A x 0.5 -S x 1.0 -w 10 -t 10 -i 1 -n 1000000 -o testrun
+ * 
+ * @author brenton
+ *
+ */
 public class FJSparkSimulator {
 	
-	
-	public static int doSimulation(CommandLine options, int s) {
+	/**
+	 * This is the method that runs on the executors.  It should run one
+	 * slice of the simulation.  It is kind-of awkward that this method
+	 * computes how big a slice should be, but it's easier that way because
+	 * I just pass in the options object.
+	 * 
+	 * @param options
+	 * @param s
+	 * @return
+	 */
+	public static FJDataAggregator doSimulation(CommandLine options) {
 		String server_queue_type = options.getOptionValue("q");
 		int num_workers = Integer.parseInt(options.getOptionValue("w"));
 		int num_tasks = Integer.parseInt(options.getOptionValue("t"));
-		long num_jobs = Long.parseLong(options.getOptionValue("n"));
+		long num_samples = Long.parseLong(options.getOptionValue("n"));
+		int num_slices = 1;
+		if (options.hasOption("s")) { num_slices = Integer.parseInt(options.getOptionValue("s")); }
 		int sampling_interval = Integer.parseInt(options.getOptionValue("i"));
 		String outfile_base = options.getOptionValue("o");
+		
+		// compute how many samples, and how many jobs are needed from each slice (round up)
+		int samples_per_slice = (int) Math.ceil( ((double)num_samples) / num_slices );
+		long jobs_per_slice = ((long) samples_per_slice) * ((long) sampling_interval);
 		
 		//
 		// figure out the arrival process
@@ -39,15 +76,15 @@ public class FJSparkSimulator {
 		IntertimeProcess service_process = FJSimulator.parseProcessSpec(service_process_spec);
 		
 		// data aggregator
-		FJDataAggregator data_aggregator = new FJDataAggregator((int)(1 + num_jobs/sampling_interval));
+		FJDataAggregator data_aggregator = new FJDataAggregator(samples_per_slice);
 		
 		// simulator
 		FJSimulator sim = new FJSimulator(server_queue_type, num_workers, num_tasks, arrival_process, service_process, data_aggregator);
 
 		// start the simulator running...
-		sim.run(num_jobs, sampling_interval);
+		sim.run(jobs_per_slice, sampling_interval);
 		
-		return 1;
+		return data_aggregator;
 	}
 	
 	public static void main(String[] args) {
@@ -59,8 +96,9 @@ public class FJSparkSimulator {
 		cli_options.addOption("q", "queuetype", true, "queue type code");
 		cli_options.addOption("w", "numworkers", true, "number of workers/servers");
 		cli_options.addOption("t", "numtasks", true, "number of tasks per job");
-		cli_options.addOption("n", "numjobs", true, "number of jobs to run");
+		cli_options.addOption("n", "numsamples", true, "number of samples to produce.  Multiply this by the sampling interval to get the number of jobs that will be run");
 		cli_options.addOption("i", "samplinginterval", true, "samplig interval");
+		cli_options.addOption("s", "numslices", true, "the number of slices to divide te job into.  This is ideally a multiple of the number of cores.");
 		cli_options.addOption(OptionBuilder.withLongOpt("outfile").hasArg().isRequired().withDescription("the base name of the output files").create("o"));
 		cli_options.addOption(OptionBuilder.withLongOpt("arrivalprocess").hasArgs().isRequired().withDescription("arrival process").create("A"));
 		cli_options.addOption(OptionBuilder.withLongOpt("serviceprocess").hasArgs().isRequired().withDescription("service process").create("S"));
@@ -76,12 +114,48 @@ public class FJSparkSimulator {
 			System.exit(0);
 		}
 		
-		System.out.println("HEY!!!");
-		
+		// we need to make the options final in order to use them in a lambda expression
 		final CommandLine foptions = options;
-		spark.parallelize(Arrays.asList(1,2),2)
-		  .map(s -> doSimulation(foptions,s))
-		  .count();
+		
+		// how many slices to divide the simulation into
+		// This doesn't have to equal the number of cores.  If there
+		// are more slices than cores they will just execute in tandem.
+		// But it is most efficient if the number of slices is a multiple
+		// of the number of cores.
+		//XXX - It would be nicer to divide this into segments equal to te number of
+		//      cores and write out data as we go.
+		int num_slices = 1;
+		if (options.hasOption("s")) { num_slices = Integer.parseInt(options.getOptionValue("s")); }
+		
+		// distribute the simulation segments to workers
+		ArrayList<Integer> ar = new ArrayList<Integer>(num_slices);
+		for (int i=0; i<num_slices; i++) { ar.add(i); }
+		JavaRDD<FJDataAggregator> rdd = spark.parallelize(ar, num_slices)
+		  .map(s -> doSimulation(foptions))
+		  .cache();
+		List<FJDataAggregator> dl = rdd.collect();
+		System.out.println("rdd = "+rdd);
+		System.out.println("dl = "+dl);
+		
+		String outfile_base = options.getOptionValue("o");
+		BufferedWriter writer = null;
+		try {
+			GZIPOutputStream zip = new GZIPOutputStream(new FileOutputStream(new File(outfile_base+"_jobdat.dat.gz")));
+			writer = new BufferedWriter(new OutputStreamWriter(zip, "UTF-8"));
+			//final BufferedWriter fwriter = writer;
+			//rdd.foreach(d -> d.appendRawJobData(fwriter));
+			for (FJDataAggregator d : dl) {
+				d.appendRawJobData(writer);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				// Close the writer regardless of what happens...
+				writer.close();
+			} catch (Exception e) {
+			}
+		}
 		
 		try {
 			Thread.sleep(1000);
