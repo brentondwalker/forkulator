f3728b546d9b859bdee55da8693523ce86df7525
Brenton Walker <brenton.d.walker@gmail.com>
2016-10-17 16:43:54 +0200

    downgrade commons-cli to 1.2 and make command line args more consistent

--- forkulator.sh
+++ forkulator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx5g -cp "bin:lib/commons-math3-3.6.1.jar:lib/commons-cli-1.3.1.jar" forkulator.FJSimulator "$@"
+java -Xmx5g -cp "bin:lib/commons-math3-3.6.1.jar:lib/commons-cli-1.2.jar" forkulator.FJSimulator "$@"
