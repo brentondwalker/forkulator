ee89af5d8a1cf05db587b55a8803006ee68c21d0
brenton <brenton.d.walker@gmail.com>
2019-04-18 09:49:45 +0200

    some changes to command line in SampleGenerator

--- src/forkulator/randomprocess/SampleGenerator.java
+++ src/forkulator/randomprocess/SampleGenerator.java
@@ -3,6 +3,8 @@ package forkulator.randomprocess;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Arrays;
 
 import org.apache.commons.cli.CommandLine;
@@ -46,10 +48,10 @@ public class SampleGenerator {
     public static void main(String[] args) {
         Options cli_options = new Options();
         cli_options.addOption("h", "help", false, "print help message");
-        cli_options.addOption("t", "numtasks", true, "number of tasks per job");
-        cli_options.addOption("w", "numworkers", true, "number of workers/servers.  This will multiply the job service times.");
+        cli_options.addOption("t", "numtasks", true, "In Job-Partition mode, the number of tasks per job");
+        cli_options.addOption("w", "numworkers", true, "In Job-Partition mode this will multiply the job service times.");
         cli_options.addOption("n", "numsamples", true, "number of samples to produce.  Multiply this by the sampling interval to get the number of jobs that will be run");
-        cli_options.addOption(OptionBuilder.withLongOpt("outfile").hasArg().isRequired().withDescription("output file to store samples").create("o"));
+        cli_options.addOption(OptionBuilder.withLongOpt("outfile").hasArg().withDescription("output file to store samples").create("o"));
         cli_options.addOption(OptionBuilder.withLongOpt("serviceprocess").hasArgs().isRequired().withDescription("service process").create("S"));
         cli_options.addOption(OptionBuilder.withLongOpt("jobpartition").hasArgs().withDescription("job_partition").create("J"));
         
@@ -59,15 +61,37 @@ public class SampleGenerator {
         try {
             options = parser.parse(cli_options, args);
         } catch (ParseException e) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("FJSimulator", cli_options);
-            e.printStackTrace();
-            System.exit(0);
+            System.out.println("\nERROR: "+e+"\n");
+            usage(cli_options);
         }
         
-        int num_tasks = Integer.parseInt(options.getOptionValue("t"));
-        int num_workers = Integer.parseInt(options.getOptionValue("w"));
-        long num_samples = Long.parseLong(options.getOptionValue("n"));
+        if (options.hasOption("h")) {
+            usage(cli_options);
+        }
+        
+        int num_tasks = 1;
+        int num_workers = 0;
+        
+        if (options.hasOption("J")) {
+            if (!options.hasOption("t") || !options.hasOption("w")) {
+                System.out.println("\nERROR: In Job-Division mode you must specify -w and -t options.\n");
+                usage(cli_options);
+            } else {
+                num_tasks = Integer.parseInt(options.getOptionValue("t"));
+                num_workers = Integer.parseInt(options.getOptionValue("w"));
+            }
+        } else {
+            if (options.hasOption("t") || options.hasOption("w")) {
+                System.out.println("\nERROR: -t and -w options can only be used in Job-Division mode.\n");
+                usage(cli_options);
+            }
+        }
+        
+        long num_samples = 100;
+        if (options.hasOption("n")) {
+            num_samples = Long.parseLong(options.getOptionValue("n"));
+        }
+        
         String outfile = options.getOptionValue("o");
 
         //
@@ -89,7 +113,12 @@ public class SampleGenerator {
 
         BufferedWriter out = null;
         try {
-            FileWriter fstream = new FileWriter(outfile, false);
+            Writer fstream;
+            if (outfile != null) {
+                fstream = new FileWriter(outfile, false);
+            } else {
+                fstream = new OutputStreamWriter(System.out);
+            }
             out = new BufferedWriter(fstream);        }
         catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
@@ -117,6 +146,7 @@ public class SampleGenerator {
             
         } else {
             // in normal mode the task service times are drawn from the service_process
+            // in this mode num_tasks and num_workers don't matter.  All samples are independent
             int batch_num = -1;
             for (int i=0; i<num_samples; i++) {
                 if ((i % num_tasks) == 0) {
@@ -142,8 +172,9 @@ public class SampleGenerator {
         }
     }
 
-    public static void usage() {
-        System.out.println("usage: UniformRandomIntervalPartition <k> <num_samples> <outfile>\n");
+    public static void usage(Options cli_options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("SampleGenerator", cli_options);
         System.exit(0);
     }
     
