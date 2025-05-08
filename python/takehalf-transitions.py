8de4f5d79f7f3152183d210ee3b10adf9eeb277d
brentondwalker <brenton.d.walker@gmail.com>
2025-05-08 16:57:47 +0200

    python script for modeling take-half parallelism distribution

---
+++ python/takehalf-transitions.py
@@ -0,0 +1,346 @@
+#!/usr/bin/env python3
+
+"""
+Compute the markov model properties for the equilibrium approximation of a take-half parallel system.
+
+S = number of workers
+B = number of busy workers
+I = number of idle workers
+F = 1/2 = take fraction (use 1/2 for now)
+
+This is a really rough approximation
+- treat each composite task as exponential, even though it is a sum of exponentials.
+- the service rate of the composite tasks is L*mu/S.
+- assume the system is in a sort of equilibrium where all the tasks currently running have the same rate.
+
+"""
+
+
+import math
+import sys
+import numpy as np
+np.set_printoptions(edgeitems=30, linewidth=100000, formatter={'float': lambda x: "{0:0.3f}".format(x)})
+import argparse
+import scipy.sparse
+import scipy.sparse.linalg
+from scipy.sparse import csr_matrix, csc_matrix, lil_matrix
+from scipy.sparse.linalg import spsolve
+import matplotlib.pyplot as plt
+
+### for older versions of python
+# from typing import Dict
+
+
+class SysState:
+    """The state of an (s,k,l) barrier-mode parallel system."""
+    __global_id_counter__ = 0
+
+    state_id = 0
+    state_len = 0
+    s = 0
+    b = 0
+    l = 0
+    transitions = None
+    visited = False
+    cycle_class = None
+
+    class Transition:
+        """A transition between two SysStates
+
+        A transition happens when one of the running tasks finishes,
+        or a new job arrives.
+        To assign the weight of this transition ..."""
+        S1 = None
+        S2 = None
+        weight = 0.0
+        job_start = False
+        task_departure = False
+
+        def __init__(self, S1, S2, weight, job_start=False, task_departure=False):
+            self.S1 = S1
+            self.S2 = S2
+            self.weight = weight
+            self.job_start = job_start
+            self.task_departure = task_departure
+
+        def __str__(self):
+            return "Transition: "+str(self.S1.l)+" --> "+str(self.S2.l)+"\tweight= "+str(self.weight)+"\tstart="+str(self.job_start)+"\tdeparture="+str(self.task_departure)
+
+
+    def __init__(self, s, l):
+        self.state_id = SysState.__global_id_counter__
+        SysState.__global_id_counter__ += 1
+        self.s = s
+        self.b = s - l
+        self.l = l
+        self.transitions = {}
+
+        # sanity check
+        print("\ncreate SysState: (S, B, L) = (", self.s, self.b, self.l, ")  ID=", self.state_id)
+
+    def __str__(self):
+        return "SysState: (s,b,l)=("+str(self.s)+","+str(self.b)+","+str(self.l)+")"
+
+    def add_transition(self, S2, weight, job_start=False, task_departure=False):
+        self.transitions[S2.l] = SysState.Transition(self, S2, weight, job_start, task_departure)
+        print("\tadd_transition ", self.l, "-->", S2.l, "  weight=",weight)
+
+
+def traverse_states(states: dict[tuple,SysState], S: SysState, lmbda, mu) -> object:
+    unvisited = {S.l}
+    states[S.l] = S
+    take_frac = 0.5
+    while len(unvisited) > 0:
+        #print("len(unvisited)=",len(unvisited), unvisited)
+        l1 = unvisited.pop()
+        S1 = states.get(l1)
+
+        # there is a task completion transition (except the zero state)
+        if S1.l < S1.s:
+            # to compute the transition rate, we need to compute the
+            # equilibrium degree of parallelism
+            # allow it to be fractional
+            print("task completion transition")
+            parallelism = min(max(S1.l*take_frac/(1-take_frac), 1), S1.s*take_frac)
+            print("\t S1 parallelism = ", parallelism)
+            l2 = S1.l + 1
+            if l2 not in states:
+                states[l2] = SysState(S1.s, l2)
+            S2 = states.get(l2)
+            # factor of S1.l because we are looking for the first task to finish out of
+            # the S1.b that are running, so it is the 1. order statistic.
+            S1.add_transition(S2, max(S1.b,1)*mu*parallelism/S1.s, task_departure=True)
+            if not S2.visited:
+                unvisited.add(l2)
+
+        # there is a job arrival transition
+        if S1.l > 0:
+            print("job arrival transition")
+            #parallelism = math.ceil(S1.l * take_frac - 1.0e-5)
+            parallelism = min(S1.l*take_frac, S1.s*take_frac)
+            print("\t S2 parallelism = ", S1.l, "*", take_frac, " = ", (S1.l*take_frac), "=",  math.ceil(S1.l*take_frac), " = ", parallelism)
+            l2 = S1.l - math.ceil(parallelism)
+            if l2 not in states:
+                states[l2] = SysState(S1.s, l2)
+            S2 = states.get(l2)
+            S1.add_transition(S2, lmbda, job_start=True)
+            if not S2.visited:
+                unvisited.add(l2)
+
+        S1.visited = True
+        print("len(unvisited) = ", len(unvisited), list(unvisited))
+
+
+def create_markov_matrix(states):
+    m = np.zeros((len(states), len(states)))
+    for vec, S in states.items():
+        print("vec:",vec, "S:",S)
+        for vec2, tr in S.transitions.items():
+            print("\tvec2:",vec2, "tr:",tr)
+            m[tr.S1.state_id, tr.S2.state_id] = tr.weight
+    return m
+
+def create_rate_matrix(states):
+    print("\ncreate_rate_matrix()")
+    Q = np.zeros((len(states), len(states)))
+    for l1, S1 in states.items():
+        print("l1:",l1, "S1:",S1)
+        for l2, tr in S1.transitions.items():
+            print("\tl2:",l2, "tr:",tr)
+            #Q[tr.S1.state_id, tr.S2.state_id] = tr.weight
+            Q[tr.S1.l, tr.S2.l] = tr.weight
+    for l, S in states.items():
+        #rs = sum(Q[S.state_id, :])
+        #Q[S.state_id, S.state_id] = -rs
+        rs = sum(Q[S.l, :])
+        Q[S.l, S.l] = -rs
+    return Q
+
+
+def matrix_power(m,n):
+    # computes m^(2^n)
+    # matrices not positive-recurrent
+    # alternating cycle of stationary distributions
+    # https://jyyuan.wordpress.com/2014/03/23/on-stationary-distributions-of-discrete-markov-chains/
+    P = m.T
+    for i in range(n):
+        P = np.matmul(P,P)
+    return P
+
+def compute_stationary_iterative(m, n):
+    P = m.T
+    size = P.shape[0]
+    pi = np.zeros(size);  pi1 = np.zeros(size)
+    pi[0] = 1;
+    for i in range(n):
+        pi[0] = 1;
+
+def compute_rate_stationary(Q):
+    dimension = Q.shape[0]
+    b = np.zeros((dimension,1))
+    Qt = Q.transpose()
+    rk = np.linalg.matrix_rank(Qt)
+    print("\ncompute_rate_stationary()\ndimension:",dimension, "\nrank:",rk, "\nb:\n",b, "\nQt:\n", Qt)
+    return np.linalg.solve(Qt,b).transpose
+
+def compute_steady_state(m):
+    # https://vknight.org/blog/posts/continuous-time-markov-chains/
+    print("compute_steady_state()")
+    dimension = m.shape[0]
+    MM = np.vstack((m.transpose()[:-1], np.ones(dimension)))
+    b = np.vstack((np.zeros((dimension - 1, 1)), [1]))
+    return np.linalg.solve(MM, b).transpose()[0]
+
+
+
+def compute_stationary_linear(m):
+    print("\nshape=",m.shape)
+    dimension = m.shape[0]
+    #MM = np.vstack((m.transpose()[:-1] - np.identity(dimension), np.ones(dimension)))
+    MM = np.vstack((m.transpose(), np.ones(dimension)))
+    print("\nMM=\n",MM)
+    b = np.vstack((np.zeros((dimension - 1, 1)), [1]))
+    print("\nb=\n",b)
+    soln = np.linalg.solve(MM, b)
+    print("\nsoln=\n",soln)
+
+def build_cycle_classes(states:dict[tuple,SysState], m, m210):
+    toler = 1e-15
+    classes = {}
+    for vec, S in states.items():
+        # we could start in this state, and apply the transition matrix until the probability
+        # of coming back is positive.  That would work in our case, but in general it is not valid,
+        # because as long as the gcd of the cycles is 1, then the stationary distr. will exist.
+        # Instead, we look at the power of the transition matrix.
+        for j in range(len(states)):
+            if m210[S.state_id, j] > toler:
+                if j not in classes:
+                    classes[j] = []
+                classes[j].append(S)
+                S.cycle_class = j
+                break
+        if S.cycle_class is None:
+            print("ERROR: state was not found in any cycle class: ", S)
+    for i in range(len(classes)):
+        print("Cycle class ",str(i))
+        for S in classes[i]:
+            print("\t",S)
+
+    # Add up the transitions between cycle classes
+    # We know already from the structure of the cycle, that all the transitions
+    # of all states of one class, go to states of the next class.
+    # Therefore, the  weight we compute here will always be the number of states in
+    # the class.  When we normalize it, it will just be a permuted identity matrix.
+    num_classes = len(classes)
+    class_transitions = np.zeros((num_classes, num_classes))
+    for vec1, S in states.items():
+        for vec2, tr in S.transitions.items():
+            class_transitions[S.cycle_class, tr.S2.cycle_class] += tr.weight
+    row_sums = class_transitions.sum(axis=1)
+    class_transitions = class_transitions / row_sums[:, np.newaxis]
+    print("Class transitions:\n",class_transitions)
+
+    return classes, class_transitions
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", '--num_workers', type=int, default=12)
+    parser.add_argument("-m", '--mu', type=float, default=1.0)
+    parser.add_argument("-l", '--lmbda', type=float, default=0.5)
+    args = parser.parse_args()
+
+    s = args.num_workers
+    mu = args.mu
+    lmbda = args.lmbda
+
+    S0 = SysState(s, s)
+    print(S0)
+    #S0.long_form()
+    states = {s: S0}
+    traverse_states(states, S0, lmbda, mu)
+
+    print("\nNumber of states: ", str(len(states)))
+    #sys.exit()
+
+    #S = SysState(s, k, l, (bmax-2,) + (0,)*(k-l))
+    #print(S.Svec)
+    #S.long_form()
+    #S = SysState(s, k, l, (bmax-2,) + (0,)*(k-l-1) + (1,))
+    #print(S.Svec)
+    #S.long_form()
+
+    #m = create_markov_matrix(states)
+    #print(m)
+    #ss = compute_steady_state(m)
+    #print("steady state:\n",ss)
+    #ssit = matrix_power(m , 10)
+    #print("stationary dist power: \n",ssit)
+    #dotted = np.dot(ssit, np.ones(ssit.shape[0]))
+    #print("\n dotted:\n", dotted)
+    #print("\n normed:\n", dotted/sum(dotted))
+
+    #print("\n  m.T * normed:\n", np.dot(m.T, dotted/sum(dotted)))
+
+    #classes, class_transitions = build_cycle_classes(states, m, ssit)
+
+    #print("\nm.T=\n",m.T)
+    ##compute_stationary_linear(m)
+
+    #evals, evecs = np.linalg.eig(m.T)
+    #evec1 = -evecs[:, np.isclose(evals, 1)]
+    ##normevec1 = evec1 / np.linalg.norm(evec1, ord=1)
+    #normevec1 = np.real(evec1 / sum(evec1))
+    #print("\nevals:\n", evals, "\nevecs:\n", evecs, "\nevec1:\n", evec1, "\nnormevec1:\n", normevec1)
+    #print("\nm.T * normevec1=\n", np.dot(m.T, normevec1))
+
+    # The rate of transitions out of the different states is different because they
+    # have different numbers of tasks running.  This is a Continuous-Time Markov Model,
+    # and we need to consider the holding time in the states.
+    # https://www.probabilitycourse.com/chapter11/11_3_2_stationary_and_limiting_distributions.php
+    #ct_stationary = np.zeros(len(states))
+    #holding_sum = 0.0
+    #for vec, S in states.items():
+    #    ct_stationary[S.state_id] = normevec1[S.state_id,0] / S.num_tasks
+    #    holding_sum += normevec1[S.state_id,0] / S.num_tasks
+    #ct_stationary /= holding_sum
+    #print("\nCT Stationary Distr:\n", ct_stationary)
+
+    # compute stationary distribution by linearly solving the CTMC rate matrix
+    Q_dense = create_rate_matrix(states)
+    #Q = create_rate_matrix_sparse(states)
+    print("\nweight matrix dense:\n", Q_dense)
+
+    ##print("\nweight matrix:\n", Q.todense())
+    ctpi_dense = compute_steady_state(Q_dense)
+
+    #ctpi_trick = compute_rate_stationary_sparse_trick(Q)
+    #ctpi = compute_rate_stationary_sparse(Q)
+    print("\n ct pi dense:\n", ctpi_dense)
+    #print("\n ct pi trick:\n", ctpi_trick)
+    #print("\n ct pi:\n", ctpi)
+    print("\n Qt * ct pi dense:\n", np.dot(Q_dense.transpose(), ctpi_dense.transpose()))
+    #print("\n Qt * ct pi:\n", Q.transpose().dot(ctpi.transpose()))
+    plt.plot(ctpi_dense)
+    plt.show()
+
+    # compute the departure rate
+    # In the backlogged steady state, what fraction of task completions lead to a job departure?
+    # In the steady state, the starting rate has to be the same as the departure rate, so we
+    # compute that too as a sanity check.
+    #XXX Made a mistake earlier and considered the product of the stationary distribution and the
+    # transition weight.  The problem is that the transition weights are normalized rel to the number
+    # of tasks running in that state, so are on different rate scales.  You can't just add them together
+    # without converting them to a common denominator.  Need to look at the raw rates, because those are
+    # in the common units of \mu (task service rate).
+    print("\nNumber of states: ", str(len(states)))
+
+
+# ======================================
+# ======================================
+# ======================================
+
+if __name__ == "__main__":
+    main()
+
